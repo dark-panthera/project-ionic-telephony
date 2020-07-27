eb167e2a7f8c8c23022829fc970ebce18f8f9be9
panthera-dark <panthera.dark@hotmail.com>
2020-07-27 17:32:35 +0200

    Portal and App Component

--- src/app/portal/portal-home/home.page.ts
+++ src/app/portal/portal-home/home.page.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscriptions } from './../subscriptions.model';
+import { Offer } from './../offer.model';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PortalService } from '../portal.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
-  protected offers$: Observable<any>;
+export class HomePage implements OnInit, OnDestroy {
+  // protected offers$: Observable<Offer[]>;
+  protected offers: Offer[];
+
+  private subOffer: Subscription;
 
   constructor(private portalService: PortalService) { }
 
   ngOnInit() {
-    this.offers$ = this.portalService.getAllOffers();
+    // this.offers$ = this.portalService.getAllOffers();
+    this.subOffer = this.portalService.getAllOffers().subscribe((offers: Offer[]) => {
+      this.offers = offers;
+    });
+
+  }
+
+  ngOnDestroy() {
+    this.subOffer.unsubscribe();
   }
 
 }
--- src/app/portal/portal.service.ts
+++ src/app/portal/portal.service.ts
@@ -1,39 +1,51 @@
-import { Offers } from './offer.model';
-import { Subscriptions } from './subscriptions.model';
+import { Offers, Offer } from './offer.model';
+import { Subscriptions, Subscription } from './subscriptions.model';
 import { Injectable } from '@angular/core';
 import { Subject, BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PortalService {
 
-  private subscriptionsBeh = new BehaviorSubject<Subscriptions[]>([]);
-  private offersBeh = new BehaviorSubject<Offers[]>([]);
+  private subscriptionsBeh = new BehaviorSubject<Subscription[]>([]);
+  private offersBeh = new BehaviorSubject<Offer[]>([]);
 
   constructor(private http: HttpClient) { }
 
-  getAllSubscriptions(): Observable<Subscriptions[]> {
-    return this.http.get<Subscriptions[]>(
+  getAllSubscriptions(): Observable<Subscription[]> {
+    return this.http.get(
       `https://selfcare-service.demo.melita.com/interview/api/offers/100/subscriptions`
-      )
+    )
       .pipe(
-        tap((subscriptions: Subscriptions[]) =>
-          this.subscriptionsBeh.next(subscriptions)
-        )
+        map((subscriptions: Subscriptions) => {
+          const sub = [];
+
+          subscriptions.subscriptions.forEach((value) => {
+            sub.push(value);
+          });
+
+          return sub;
+        })
       );
   }
 
-  getAllOffers(): Observable<Offers[]> {
-    return this.http.get<Offers[]>(
+  getAllOffers(): Observable<Offer[]> {
+    return this.http.get(
       `https://selfcare-service.demo.melita.com/interview/api/offers`
-      )
+    )
       .pipe(
-        tap((offers: Offers[]) =>
-          this.offersBeh.next(offers)
-        )
+        map((offers: Offers) => {
+          const sub = [];
+
+          offers.offers.forEach((value) => {
+            sub.push(value);
+          });
+
+          return sub;
+        })
       );
   }
 
